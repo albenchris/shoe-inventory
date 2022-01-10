286f3844501e354d24cfef0ba51db1efb385afaa
albenchris <albenchris00@gmail.com>
2022-01-10 13:39:23 -0600

    initial ShoeDao

---
+++ app/src/main/java/com/example/shoeinventorymanagement/data/ShoeDao.kt
@@ -0,0 +1,18 @@
+package com.example.shoeinventorymanagement.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ShoeDao {
+
+    @Query("SELECT * FROM shoe_table ORDER BY brand ASC")
+    fun getAllShoes() : Flow<List<Shoe>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(shoe: Shoe)
+
+    @Query("DELETE FROM shoe_table")
+    suspend fun deleteAll()
+    
+}
---
+++ app/src/main/java/com/example/shoeinventorymanagement/data/ShoeRoomDatabase.kt
@@ -0,0 +1,7 @@
+package com.example.shoeinventorymanagement.data
+
+import androidx.room.RoomDatabase
+
+abstract class ShoeRoomDatabase : RoomDatabase() {
+
+}
