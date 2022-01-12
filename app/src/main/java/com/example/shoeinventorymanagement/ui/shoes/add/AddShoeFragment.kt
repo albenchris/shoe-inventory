3420f18f034650cdcac4220f10f20dd6123ddf0f
albenchris <albenchris00@gmail.com>
2022-01-12 10:58:59 -0600

    added button to add_shoe_fragment

--- app/src/main/java/com/example/shoeinventorymanagement/ui/shoes/add/AddShoeFragment.kt
+++ app/src/main/java/com/example/shoeinventorymanagement/ui/shoes/add/AddShoeFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.example.shoeinventorymanagement.R
 
 class AddShoeFragment : Fragment() {
+
+    private lateinit var addBrandView : EditText
+    private lateinit var addTypeView : EditText
+    private lateinit var addPriceView : EditText
 
     companion object {
         fun newInstance() = AddShoeFragment()
@@ -20,13 +25,19 @@ class AddShoeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.add_shoe_fragment, container, false)
+        var view : View = inflater.inflate(R.layout.add_shoe_fragment, container, false)
+
+        addBrandView = view.findViewById(R.id.add_brand)
+        addTypeView = view.findViewById(R.id.add_type)
+        addPriceView = view.findViewById(R.id.add_price)
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AddShoeViewModel::class.java)
-        // TODO: Use the ViewModel
+//        viewModel = ViewModelProvider(this).get(AddShoeViewModel::class.java)
+//        // TODO: Use the ViewModel
     }
 
 }
